b0b83d43531120683144d92da9c343b22166be14
Steljo Shaji <shajisteljo@gmail.com>
2023-09-25 12:49:20 +0530

    Simple Interest App

--- src/App.js
+++ src/App.js
@@ -72,50 +72,42 @@ function App() {
       <p className='mb-1 me-2'>Total Simple Interest</p>
 
       </div>
-      <form className="mt-5"onSubmit={handleCalculate}>
-        <div className="mb-3 ">
-        <TextField className='w-100' id="outlined-basic" label="₹ Principal Amount" variant="outlined"
-         value={Principle || ""} name='principle' onChange={(e)=>validateUserInput(e)} />
-         </div>
-
-       {
-       !validPrinciple  &&
-       <div className='mb-3 text-danger fw-bolder'>
+      <form className="mt-5" onSubmit={handleCalculate}>
+        <div className="mb-3">
+        <TextField className='w-100' id="outlined-basic1" label="₹Principle Amount" variant="outlined" value={Principle || ""} name='principle' onChange={(e)=>validateUserInput(e)} />
+        </div>
+        {
+        !validPrinciple &&
+        <div className="mb-3 text-danger fw-bolder">
           *Invalid Principle Amount
-         </div>}
+        </div>
+        }
 
         <div className="mb-3">
-        <TextField className='w-100' id="outlined-basic" label="Rate of Interest (p.a) %" variant="outlined" 
-        value={rate || ""} name='rate' onChange={(e)=>validateUserInput(e)}/>
-
-{
-       !validRate  &&
-       <div className='mb-3 text-danger fw-bolder'>
+        <TextField className='w-100' id="outlined-basic2" label="Rate of Interest (p.a) %" variant="outlined" value={rate || ""} name='rate' onChange={(e)=>validateUserInput(e)}/>
+        </div>
+        {
+        !validRate &&
+        <div className="mb-3 text-danger fw-bolder">
           *Invalid Rate Amount
-         </div>}
-
         </div>
+        }
+
         <div className="mb-5">
-        <TextField className='w-100' id="outlined-basic" label="Time Period (Yr)" variant="outlined" 
-        value={year || ""} name='year' onChange={(e)=>validateUserInput(e)}/>
-
-{
-       !validYear  &&
-       <div className='mb-3 text-danger fw-bolder'>
-          *Invalid Year
-         </div>}
-
-
+        <TextField className='w-100' id="outlined-basic3" label="Time period (Yr)" variant="outlined" value={year || ""} name='year' onChange={(e)=>validateUserInput(e)}/>
         </div>
-        <Stack className='mt-2' direction="row" spacing={2}>
-        <Button type='submit' style={{height:'70px',width:'250px'}} className='bg-dark' variant="contained" 
-        disabled={validPrinciple && validRate && validYear?false:true} >Calculate</Button>
-        <Button onClick={handleReset}  style={{height:'70px',width:'250px'}}  variant="outlined">Reset</Button>
-  
+        {
+        !validYear &&
+        <div className="mb-3 text-danger fw-bolder">
+          *Invalid Year
+        </div>
+        }
+        <Stack direction="row" spacing={2}>
+        <Button type='submit' style={{height:'70px',width:'250px'}} variant="contained" disabled={validPrinciple && validRate && validYear?false:true} >Calculate</Button>
+        <Button onClick={handleReset} style={{height:'70px',width:'250px'}} variant="outlined">reset</Button>
         </Stack>
 
-      </form>
-
+       </form>
       </div>
     
     </div>
